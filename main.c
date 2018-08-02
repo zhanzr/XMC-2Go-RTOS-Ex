0f0df9f819c8f32df8dd3abce36dbb81f384a06f
zhanzr <zhanzr@foxmail.com>
2018-08-03 02:37:31 +0800

    Barrier Turnstile

--- main.c
+++ main.c
@@ -17,6 +17,7 @@
 #include <stdlib.h>
 #include <string.h>
 #include <stdio.h>
+#include <stdarg.h>
 
 #include <XMC1100.h>
 #include <xmc_scu.h>
@@ -31,16 +32,15 @@
 
 #define LED1 P1_0
 #define LED2 P1_1
-#define BLINK_DELAY_N	100000
-#define BLINK_DELAY_TICK	100
+#define BLINK_DELAY_TICK	500
 #define DTS_SAMPLE_TICK	400
 
-#define LED_SIGNAL_1	0x01
-#define LED_SIGNAL_2	0x02
+#define THREAD_NUM	3
 
 void test_Thread (void const *argument);
+void test_Thread3 (void const *argument);
 
-osThreadDef(test_Thread, osPriorityNormal, 3, 0);
+osThreadDef(test_Thread, osPriorityNormal, THREAD_NUM, 0);
 
 osThreadId T_thread_ID_1;																			
 osThreadId T_thread_ID_2;
@@ -53,8 +53,11 @@ osSemaphoreDef(sem1);
 osSemaphoreId sem2;									
 osSemaphoreDef(sem2);
 
-osSemaphoreId Mutex;									
-osSemaphoreDef(Mutex);
+osSemaphoreId sem_cnt_protect;									
+osSemaphoreDef(sem_cnt_protect);
+
+osMutexId uart_mutex;
+osMutexDef (uart_mutex);
 
 static uint32_t g_cnt;
 
@@ -101,6 +104,26 @@ int stdout_putchar(int ch)
 	}
 	return ch;
 }
+
+int mutex_printf (const char *format, ...)
+{
+	osMutexWait(uart_mutex, osWaitForever);
+	
+  va_list args;
+  int retval;
+
+  va_start (args, format);
+	
+  retval = vfprintf (stdout, format, args);	
+	
+  va_end (args);
+	
+	osMutexRelease(uart_mutex);
+	
+  return retval;
+}
+
+
 
 /* Initialize peripherals */
 void LED_Initialize(void)
@@ -161,58 +184,62 @@ void LED_Toggle(uint8_t n)
 	}
 }
 
-void test_Thread (void const *argument) 
+void test_Thread(void const *argument) 
 {
 	uint32_t para = (uint32_t)argument;
 	
 	for (;;) 
-	{		
-		osSemaphoreWait(Mutex, osWaitForever);
+	{				
+		osSemaphoreWait(sem_cnt_protect, osWaitForever);
 		g_cnt ++;
-		if(3==g_cnt)
+		if(THREAD_NUM==g_cnt)
 		{
 			osSemaphoreWait(sem2, osWaitForever);					
 			osSemaphoreRelease(sem1);					
 		}			
-		osSemaphoreRelease(Mutex);	
+		osSemaphoreRelease(sem_cnt_protect);	
 		
+		//The gate entry
 		osSemaphoreWait(sem1, osWaitForever);					
-		osSemaphoreRelease(sem2);		
+		osSemaphoreRelease(sem1);		
 			
 		switch(para)
 		{
 			case 1:
 			delta_tick1 = osKernelSysTick() - old_tick1;
 			old_tick1 = osKernelSysTick();
+			osDelay(BLINK_DELAY_TICK * para);
 			break;		
 			
 			case 2:
 			delta_tick2 = osKernelSysTick() - old_tick2;
 			old_tick2 = osKernelSysTick();
+			osDelay(BLINK_DELAY_TICK * para);
 			break;	
-			
+
 			case 3:
 			delta_tick3 = osKernelSysTick() - old_tick3;
 			old_tick3 = osKernelSysTick();
 			printf("%u %u %u\n", delta_tick1, delta_tick2, delta_tick3);		
-			break;	
-
+			osDelay(BLINK_DELAY_TICK * para);
+			break;
+			
 			default:
 				break;
 		}
-		osDelay(BLINK_DELAY_TICK);
 
-		osSemaphoreWait(Mutex, osWaitForever);
+		osSemaphoreWait(sem_cnt_protect, osWaitForever);
 		g_cnt --;
 		if(0==g_cnt)
 		{
 				osSemaphoreWait(sem1, osWaitForever);					
 				osSemaphoreRelease(sem2);				
 		}
-		osSemaphoreRelease(Mutex);	
+		osSemaphoreRelease(sem_cnt_protect);	
 		
+		//The gate exit
 		osSemaphoreWait(sem2, osWaitForever);					
-		osSemaphoreRelease(sem1);			
+		osSemaphoreRelease(sem2);			
 	}	
 }
 
@@ -254,12 +281,14 @@ int main(void)
 	
 	sem1 = osSemaphoreCreate(osSemaphore(sem1), 0);	
 	sem2 = osSemaphoreCreate(osSemaphore(sem2), 1);	
-	Mutex = osSemaphoreCreate(osSemaphore(sem1), 1);	
+	sem_cnt_protect = osSemaphoreCreate(osSemaphore(sem_cnt_protect), 1);	
+	
+	uart_mutex = osMutexCreate(osMutex(uart_mutex));
 	
 	T_thread_ID_1 = osThreadCreate(osThread(test_Thread), (void*)1);	
 	T_thread_ID_2 = osThreadCreate(osThread(test_Thread), (void*)2);
 	T_thread_ID_3 = osThreadCreate(osThread(test_Thread), (void*)3);
-
+	
 	old_tick1 = old_tick2 = old_tick3 = osKernelSysTick();
 
 	osKernelStart ();                         // start thread execution 
