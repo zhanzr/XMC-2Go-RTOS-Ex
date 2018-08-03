f039b3e51702094b5d2d7b1ed621da5cc7635993
zhanzr <zhanzr@foxmail.com>
2018-08-03 23:19:48 +0800

    mutex demo.

--- main.c
+++ main.c
@@ -33,39 +33,20 @@
 #define LED1 P1_0
 #define LED2 P1_1
 #define BLINK_DELAY_TICK	500
-#define DTS_SAMPLE_TICK	400
 
-#define THREAD_NUM	3
+void uart_Thread1 (void const *argument);
+void uart_Thread2 (void const *argument);
 
-void test_Thread (void const *argument);
-
-osThreadDef(test_Thread, osPriorityNormal, THREAD_NUM, 0);
+osThreadDef(uart_Thread1, osPriorityNormal, 1, 0);
+osThreadDef(uart_Thread2, osPriorityNormal, 1, 0);
 
 osThreadId T_thread_ID_1;																			
 osThreadId T_thread_ID_2;
-osThreadId T_thread_ID_3;
 /*----------------------------------------------------------------------------
-  Define the semaphore
+  Define the mutex
  *---------------------------------------------------------------------------*/	
-osSemaphoreId sem_entry;									
-osSemaphoreDef(sem_entry);
-osSemaphoreId sem_exit;									
-osSemaphoreDef(sem_exit);
-
-osSemaphoreId sem_cnt_protect;									
-osSemaphoreDef(sem_cnt_protect);
-
 osMutexId uart_mutex;
 osMutexDef (uart_mutex);
-
-static uint32_t g_cnt;
-
-static uint32_t old_tick1;
-static uint32_t old_tick2;
-static uint32_t old_tick3;
-static uint32_t delta_tick1;
-static uint32_t delta_tick2;
-static uint32_t delta_tick3;
 
 extern ARM_DRIVER_USART Driver_USART0;
 static ARM_DRIVER_USART *UARTdrv = &Driver_USART0; 
@@ -121,8 +102,6 @@ int mutex_printf (const char *format, ...)
 	
   return retval;
 }
-
-
 
 /* Initialize peripherals */
 void LED_Initialize(void)
@@ -183,63 +162,31 @@ void LED_Toggle(uint8_t n)
 	}
 }
 
-void test_Thread(void const *argument) 
+
+/*----------------------------------------------------------------------------
+  Thread one writes to UART
+ *---------------------------------------------------------------------------*/
+void uart_Thread1 (void const *argument) 
 {
-	uint32_t para = (uint32_t)argument;
-	
 	for (;;) 
-	{				
-		osSemaphoreWait(sem_cnt_protect, osWaitForever);
-		g_cnt ++;
-		if(THREAD_NUM==g_cnt)
-		{
-			osSemaphoreWait(sem_exit, osWaitForever);					
-			osSemaphoreRelease(sem_entry);					
-		}			
-		osSemaphoreRelease(sem_cnt_protect);	
-		
-		//The gate entry
-		osSemaphoreWait(sem_entry, osWaitForever);					
-		osSemaphoreRelease(sem_entry);		
-			
-		switch(para)
-		{
-			case 1:
-			delta_tick1 = osKernelSysTick() - old_tick1;
-			old_tick1 = osKernelSysTick();
-			osDelay(BLINK_DELAY_TICK * para);
-			break;		
-			
-			case 2:
-			delta_tick2 = osKernelSysTick() - old_tick2;
-			old_tick2 = osKernelSysTick();
-			osDelay(BLINK_DELAY_TICK * para);
-			break;	
+	{
+		//printf("%s %u\n", __func__, osKernelSysTick());
+		mutex_printf("%s %u\n", __func__, osKernelSysTick());
+		osDelay(BLINK_DELAY_TICK);
+	}
+}
 
-			case 3:
-			delta_tick3 = osKernelSysTick() - old_tick3;
-			old_tick3 = osKernelSysTick();
-			printf("%u %u %u\n", delta_tick1, delta_tick2, delta_tick3);		
-			osDelay(BLINK_DELAY_TICK * para);
-			break;
-			
-			default:
-				break;
-		}
-
-		osSemaphoreWait(sem_cnt_protect, osWaitForever);
-		g_cnt --;
-		if(0==g_cnt)
-		{
-				osSemaphoreWait(sem_entry, osWaitForever);					
-				osSemaphoreRelease(sem_exit);				
-		}
-		osSemaphoreRelease(sem_cnt_protect);	
-		
-		//The gate exit
-		osSemaphoreWait(sem_exit, osWaitForever);					
-		osSemaphoreRelease(sem_exit);			
-	}	
+/*----------------------------------------------------------------------------
+  Thread two writes to UART
+ *---------------------------------------------------------------------------*/
+void uart_Thread2 (void const *argument) 
+{
+	for(;;)
+	{
+		//printf("%s %u\n", __func__, osKernelSysTick());
+		mutex_printf("%s %u\n", __func__, osKernelSysTick());
+		osDelay(BLINK_DELAY_TICK);
+	}
 }
 
 void UART_Init(void)
@@ -268,7 +215,7 @@ int main(void)
 	
 	UART_Init();
 
-	printf("2Go Barrier Turnstile @ %u Hz %s\n", 
+	printf("2Go Mutex @ %u Hz %s\n", 
 	SystemCoreClock, 
 	osKernelSystemId);
 	
@@ -277,18 +224,11 @@ int main(void)
 	#else
 	printf("StandardLib\n");
 #endif
-	
-	sem_entry = osSemaphoreCreate(osSemaphore(sem_entry), 0);	
-	sem_exit = osSemaphoreCreate(osSemaphore(sem_exit), 1);	
-	sem_cnt_protect = osSemaphoreCreate(osSemaphore(sem_cnt_protect), 1);	
-	
+
 	uart_mutex = osMutexCreate(osMutex(uart_mutex));
 	
-	T_thread_ID_1 = osThreadCreate(osThread(test_Thread), (void*)1);	
-	T_thread_ID_2 = osThreadCreate(osThread(test_Thread), (void*)2);
-	T_thread_ID_3 = osThreadCreate(osThread(test_Thread), (void*)3);
+	T_thread_ID_1 = osThreadCreate(osThread(uart_Thread1), (void*)1);	
+	T_thread_ID_2 = osThreadCreate(osThread(uart_Thread2), (void*)2);
 	
-	old_tick1 = old_tick2 = old_tick3 = osKernelSysTick();
-
 	osKernelStart ();                         // start thread execution 
 }
